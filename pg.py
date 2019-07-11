9eb8a06b15789d5265c1031ec74c6a7e568f1d37
Abner Anjos <33687984+AbnerErnaniADSFatec@users.noreply.github.com>
2019-07-11 13:43:32 -0300

    Update pg.py

--- pg.py
+++ pg.py
@@ -1,4 +1,4 @@
-pgHost = "127.0.0.1" # "terrama2.dpi.inpe.br" # "150.163.2.217"
+pgHost = "127.0.0.1"
 pgPort = "5432"
-pgUser = "postgres" # "chuva"
-pgPassWord = "postgres" # "secreto"
+pgUser = "postgres"
+pgPassWord = "postgres"
